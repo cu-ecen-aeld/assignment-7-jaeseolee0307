aef61d417061ab4ae6493a6e2f082cd819993e77
jaeseolee0307 <jaeseolee0307@gmail.com>
2024-05-26 22:01:47 +0900

    modify misc-modules

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -5,15 +5,17 @@
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+#define GITHUB_USER "jaeseolee0307"
+
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world // %s\n", GITHUB_USER);
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world // %s\n", GITHUB_USER);
 }
 
 module_init(hello_init);
